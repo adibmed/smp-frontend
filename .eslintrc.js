c8d0730982e369c93faac17a1291184508187c72
adibemohamed <mr.eladib.mohamed@gmail.com>
2022-02-24 01:02:55 +0100

    fix: prettier & eslint conflict

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,12 +10,23 @@ module.exports = {
   },
   extends: [
     '@nuxtjs',
+     'prettier/vue',
     'plugin:nuxt/recommended'
   ],
   plugins: [
+    'prettier'
   ],
   // add your custom rules here
   rules: {
-    'vue/multi-word-component-names': 'off'
+    'prettier/prettier': 'error',
+    'no-console': 0,
+    'vue/html-self-closing': 0,
+    'vue/no-v-html': 0,
+    'vue/valid-v-slot': 0,
+    'vue/valid-template-root': 0,
+    'vue/multi-word-component-names': 0,
+    'vue/attributes-order': 0,
+    'vue/script-indent': 0,
+    'vue/require-default-prop': 0
   }
 }
